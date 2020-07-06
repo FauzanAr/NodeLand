f7763232b40662987aeb72a457b0b20046f48d22
FauzanAr <faurinta12345@gmail.com>
2020-07-06 18:13:07 +0700

    set-up morgan

--- API/app.js
+++ API/app.js
@@ -1,17 +1,31 @@
 const express = require('express')
 const dbConnection = require('./config/db')
+const dotenv = require('dotenv')
+const morgan = require('morgan')
 
-//* Connection to Database
+
+// *Adding dotenv config
+dotenv.config({
+    path: './config/.env'
+})
+// *Connection to Database
 dbConnection()
 
+// *Initializing express on an app
 const app = express()
 
+// !Please remove 'dev' if it in production mode
+app.use(morgan('dev'))
+
+// *Adding body-parser build by express
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 
 app.get('/',(req, res) => {
     res.send('first up')
 })
 
-//* Listen port
+// *Listen port
 app.listen(process.env.PORT, () => {
     console.log(`Serving on ${process.env.PORT}`)
 })
